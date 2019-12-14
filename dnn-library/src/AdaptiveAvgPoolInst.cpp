8dc29dc3eb60901c10d4ce6d22ac1777ea08518f
Rafah Hajjar Muñoz <rafah.hajjar@esperantotech.com>
2019-12-14 12:00:15 -0800

    [dnn-library] [SW-815] Adding Lib AdaptiveAvgPool Node implementation

---
+++ dnn-library/src/AdaptiveAvgPoolInst.cpp
@@ -0,0 +1,95 @@
+/*-------------------------------------------------------------------------
+ * Copyright (C) 2019, Esperanto Technologies Inc.
+ * The copyright to the computer program(s) herein is the
+ * property of Esperanto Technologies, Inc. All Rights Reserved.
+ * The program(s) may be used and/or copied only with
+ * the written permission of Esperanto Technologies and
+ * in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ *-------------------------------------------------------------------------
+ */
+
+#include <assert.h>
+#include <fenv.h>
+#include <limits>
+#include <cmath>
+#include <cstring>
+
+#include "LibNodes.h"
+#include "GenInstances.h"
+#include "Float16.h"
+#include "Writer.h"
+#include "Addresser.h"
+#include "Converter.h"
+#include "Operator.h"
+#include "utils.h"
+
+using namespace std;
+
+template <typename srcType>
+void dnn_lib::fwdLibAdaptiveAvgPoolInst(void *dstMatrix, void *dstMatrixDims,
+                                void *dstMatrixPitches, void *activations,
+                                void *activationsDims, void *activationsPitches,
+                                float *scale, int32_t *offset) {
+
+  unsigned int minionId = get_minion_id();
+  if (minionId != 0)
+    return;
+
+  Addresser<srcType> tOutput(dstMatrix, scale[1], offset[1]);
+  const Addresser<srcType> tAInput(activations, scale[0], offset[0]);
+
+  unsigned int *dstIndex = (unsigned int *)dstMatrixDims;
+  unsigned int *actIndex = (unsigned int *)activationsDims;
+
+  unsigned int *dstPitch = (unsigned int *)dstMatrixPitches;
+  unsigned int *actPitch = (unsigned int *)activationsPitches;
+
+#define START_IND(a, b, c) (a * c) / b
+#define END_IND(a, b, c) ((a + 1) * c - 1) / b + 1
+
+  // For each input in the batch:
+  for (size_t n = 0; n < dstIndex[0]; n++) {
+    // For each layer in the output tensor:
+    for (size_t z = 0; z < actIndex[3]; z++) {
+      // For each value in the output tensor::
+      for (size_t ax = 0; ax < dstIndex[1]; ax++) {
+
+        unsigned int x = START_IND(ax, dstIndex[1], actIndex[1]);
+        unsigned int kH = END_IND(ax, dstIndex[1], actIndex[1]) - x;
+
+        for (size_t ay = 0; ay < dstIndex[2]; ay++) {
+
+          unsigned int y = START_IND(ay, dstIndex[2], actIndex[2]);
+          unsigned int kW = END_IND(ay, dstIndex[2], actIndex[2]) - y;
+          
+          float sum = tAInput[0];
+          sum = 0;
+
+          for (size_t fx = 0; fx < kH; fx++) {
+            for (size_t fy = 0; fy < kW; fy++) {
+              unsigned int ox = x + fx;
+              unsigned int oy = y + fy;
+
+              sum += tAInput[n * actPitch[0] + (size_t)ox * actPitch[1] +
+                             (size_t)oy * actPitch[2] + z * dstPitch[3]];
+            }
+          }
+
+          float kHW = kH * kW;
+          float invkHW;
+          fpReciprocalSingleElement(kHW, invkHW);
+          tOutput[n * dstPitch[0] + (size_t)ax * dstPitch[1] +
+                  (size_t)ay * dstPitch[2] + z * dstPitch[3]] = sum * invkHW;
+        } // W
+      }   // H
+    }     // C
+  }       // N
+#undef START_IND
+#undef END_IND
+}
+
+GEN_INSTANCES_OP(template, fwdLibAdaptiveAvgPoolInst,void *dstMatrix, void *dstMatrixDims,
+                 void *dstMatrixPitches, void *activations,
+                 void *activationsDims, void *activationsPitches,
+                 float *scale, int32_t *offset);
