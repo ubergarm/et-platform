fdc41c9c3112eb2898de1c1c3fb2d67eaf775e19
Guillem Sole <guillem.sole@esperantotech.com>
2020-02-11 05:19:00 -0800

    [dnn-library] [SW-1867]   - Added defines of sync minions in shire.h     in lib   - Moved cb_drain function to cacheops.h   - Removing the dump of sync, syncopy, alloc and dealloc tensors as they     do not change any content for regular tensors   - Several fixes in sync copy function to work correctly     * Added barriers to guarantee correctness     * Fixed infinite loops due not re-init values that are       reused     * Added coallescing buffer drain to guarantee data is       in L3   - LibManager doesn't need to consume a credit for SyncCopy instruction     as this function is executed only in sync minions and credit is     consumed outside   - Allowed capability to global barriers to skip the initial barrier     or the credit send by checking if some arguments are all 0s      * This is needed for sync copy support, where the credit receive        from compute minions and credit sent to compute minions is        now split   - UberKernel now splits the global barrier in sync minions in two     phases when doing sync copy     * First it receives the credits from all shires     * When done it executes the copy     * When done it sends the credits back to compute shires   - Correctly sending the tensor addresses to copy fro sync copy     nodes in uberkernel generation   - Added field in EInstruction to know it is the last sync copy of     a group of sync copies   - The last sync copy generates a final barrier and sends credit     back to compute shires   - Tensor allocator (non-smart) was allocating same tensor several     times because it was not ignoring sync, sync copies and non-allocs   - Update memory map in tensor allocator was not computing correctly     total size as it was not counting for extra space due alignment   - Updated function that dumps the contents of the copies of the     tensors for intermediate debugging     * Dumping contents in JSON     * Dumping more info to be able to extract tensors in an off-line       program: size, pitch, precision, ...     * Dumping original names to easily match with interpreter   - Removing the DBG intermediate file on txt dump

--- dnn-library/src/SyncopyInstTensorized.cpp
+++ dnn-library/src/SyncopyInstTensorized.cpp
@@ -23,6 +23,8 @@
 #include "Converter.h"
 #include "Operator.h"
 #include "utils.h"
+#include "shire.h"
+#include "barriers.h"
 
 using namespace std;
 
@@ -48,44 +50,83 @@ using namespace std;
                                            float *scale, int32_t *offset,
                                            unsigned int off) {
 
-  int32_t minionId = get_minion_id() - (32 * 32 + 16);
+  uint32_t hart = get_hart_id();
+  uint32_t threadId = hart & 1;
+  uint32_t minionId = (hart >> 1) - (32 * 32 + 16);
   int32_t activeMinions = 16;
-  if ((minionId < 0) || (minionId >= activeMinions))
-    return;
+  // Disable second thread from now on, as they don't have tensor extensions
+  if (threadId != 0) { return; }
 
-  unsigned int *Index = (unsigned int *)Dims;
-  unsigned int *Pitch = (unsigned int *)Pitches;
+  unsigned int *Index = (unsigned int *) Dims;
+  unsigned int *Pitch = (unsigned int *) Pitches;
 
   size_t typeSize = getsize<srcType>();
-  uint64_t numElems = Pitch[0] * Index[0] *
-                             typeSize + off; // Total number of elements in the tensor
-  uint64_t numCacheLines = (numElems - 1) / 64 + 1; // 64 = CacheLineLength
-  uint64_t minionCacheLines = (numCacheLines - 1) / activeMinions + 1;
+  uint64_t numBytes = Pitch[0] * Index[0] * typeSize + off; // Total number of elements in the tensor
+  uint64_t numCacheLines = (numBytes - 1) / 64 + 1; // 64 = CacheLineLength
+  int64_t  minionCacheLines = (numCacheLines - 1) / activeMinions + 1;
   uint64_t initialCacheLine = minionCacheLines * minionId;
   uint64_t lastCacheLine = initialCacheLine + minionCacheLines;
-  minionCacheLines =
-          (lastCacheLine <= numCacheLines) ? minionCacheLines
-        : (initialCacheLine < numCacheLines) ? numCacheLines - initialCacheLine : 0;
-  uint64_t srcAddr = (uint64_t)src + initialCacheLine*64;
-  uint64_t dstAddr = (uint64_t)dst + initialCacheLine*64;
+  minionCacheLines = (lastCacheLine <= numCacheLines)   ? minionCacheLines
+                   : (initialCacheLine < numCacheLines) ? numCacheLines - initialCacheLine
+                   :                                      0;
 
+  // Computes source and destination
+  uint64_t srcAddr = ((uint64_t) src & ~0x3FULL) + initialCacheLine * 64; // Aligns to cache line
+  uint64_t dstAddr = (uint64_t) dst + initialCacheLine * 64;
+  uint64_t dstAddrOrig = dstAddr;
+
+  // Saves minion cache lines for evicts
+  int64_t minionCacheLinesOrig = minionCacheLines;
+
+  // Actual copies doing tensorload + tensorstore
   __asm__ __volatile__("mov.m.x m0, zero, 0xff \n");
   while (minionCacheLines > 0) {
+    // Caps to 16 cachelines
     uint32_t cl = (minionCacheLines >= 16) ? 0xF : minionCacheLines-1;
+    // Copies source to L1 Scp and makes sure they are available
+    // for the tensor store
     tensor_load(0, 0, 0, 0, 0, srcAddr, 0, cl, 0x40, 0);
     WAIT_TENSOR_LOAD_0;
-    srcAddr += 1024;
-    minionCacheLines -= 16;
+    // Need to wait for TStore to prevent overriding L1 scp next iteration
+    // with the tensor loads
     tensor_store_scp(0, 0, cl, dstAddr, 0x40);
+    WAIT_TENSOR_STORE;
+    // Update for next iteration
+    minionCacheLines -= 16;
+    srcAddr += 1024;
     dstAddr += 1024;
   }
-  WAIT_TENSOR_STORE;
-  // flush CB
-  dstAddr = (uint64_t)dst + initialCacheLine*64;
+
+  // Barrier to get all minions threa0 here
+  // Guarantees all data in CB
+  shire_barrier(0,                 // FLB0
+                0,                 // FCC0
+                activeMinions,     // Number of active minions
+                SYNC_MINIONS_MASK, // Mask of active thread0 minions
+                0);                // Mask of active thread1 minions
+
+  // Flush CB (4 banks, 4 minions in parallel)
+  if(minionId < 4)
+    cb_drain(SHIRE_OWN, minionId);
+
+  // Barrier to get all minions threa0 here
+  // Guarantees all data in L3
+  shire_barrier(0,                 // FLB0
+                0,                 // FCC0
+                activeMinions,     // Number of active minions
+                SYNC_MINIONS_MASK, // Mask of active thread0 minions
+                0);                // Mask of active thread1 minions
+
+  // Evicts from L3 to DDR
+  minionCacheLines = minionCacheLinesOrig;
+  dstAddr = dstAddrOrig;
   while (minionCacheLines > 0) {
+    // Caps to 16 cachelines
     uint32_t cl = (minionCacheLines >= 16) ? 0xF : minionCacheLines-1;
-    // flush L3 to DDR
+    // Flush L3 to DDR
     evict_va_multi(0x3, dstAddr, cl);
+    // Update for next iteration
+    minionCacheLines -= 16;
     dstAddr += 1024;
   }
   WAIT_CACHEOPS;
