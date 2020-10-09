ce677893d3428de3c9d3051b6c644fa1b69512b3
Guillem Sole <guillem.sole@esperantotech.com>
2020-10-09 07:33:50 -0700

    [dnn-library] [SW-4589] Copy instruction hits coherency issues   - Switching copy baseline instruction to an already existing version     that was removed   - This version doesn't write on the padding region   - This version prevents having two minions writing the same cacheline

--- dnn-library/include/inlining/CopyInst.h
+++ dnn-library/include/inlining/CopyInst.h
@@ -180,16 +180,12 @@ inline void copyBytes(uint8_t * src,
   }
 }
 
-
 /**
  * @brief Copies the src tensor to the dst tensor.
  *
  * It makes a copy of the tensor src into the dst tensor, which may not
  * have the same pitches or dimensions, so it allows a reshaping. This is
- * the threaded and vectorized version for this operator.
- *
- * @Warning It is assumed that the destination tensor starts at the beginning
- *  of a cacheline.
+ * the threaded version for this operator, so several minions are used.
  * 
  * @tparam srcType The type of the elements in the tensor.
  * @param[out] dst Pointer to the output tensor.
@@ -207,70 +203,45 @@ inline void copyBytes(uint8_t * src,
  */
 template <ElemKind elK>
 inline void fwdLibCopyInst(LibTensor* outT, LibTensor* inT,
-                                     uint64_t flags,
-                                     const uint32_t minionOffset = 0,
-                                     const uint32_t assignedMinions = 0) {
+                                   uint64_t flags,
+                                   const uint32_t minionOffset = 0,
+                                   const uint32_t assignedMinions = 0) {
   using srcType = typename elemKind2elemTy<elK>::type;
   unsigned int minionId = get_minion_id() - minionOffset;
-  
-  // TODO: REMOVE ONCE SW-4190 is fixed
-  if (outT->getUntouchable()) {
-    if (minionId == minionOffset) {
-      CopyInstScalar<elK>(outT, inT, flags, minionOffset, assignedMinions);
-    }
-    return;
-  }
-  // TODO: END REMOVE ONCE SW-4190 is fixed
-  
   unsigned int activeMinions = (assignedMinions == 0) ? (MIN_PER_SHIRE * ACTIVE_SHIRES) : assignedMinions;
   if (minionId >= activeMinions)
     return;
 
   /* maintain compatibility through the new Iface Libtensor */
-  void* src = inT->getRawDataPointer<void>();
-  void* dst = outT->getRawDataPointer<void>();
- 
-  // Addresser<elK> tOutput(dst, scale[1], offset[1]);
-  Addresser<elK> tOutput(dst, outT->getScale(), outT->getOffset());
-  // const Addresser<elK> tAInput(src, scale[0], offset[0]);
-  const Addresser<elK> tInput(src, inT->getScale(), inT->getOffset());
   
-  //  unsigned int *dstIndex = (unsigned int *)dstDims;
+  uint8_t * src = (uint8_t *) inT->getRawDataPointer<void>();
+  uint8_t * dst = (uint8_t *) outT->getRawDataPointer<void>();
+  
   const dim_t *dstIndex = outT->dims().data();
-  
-  // unsigned int *actIndex = (unsigned int *)srcDims;
   const dim_t *actIndex = inT->dims().data();
   
-  uint8_t *dst8 = (uint8_t *)dst;
-  uint8_t *src8 = (uint8_t *)src;
-  uint8_t *src8Init = src8;
-  uint8_t *dst8Init = dst8;
-
-  // unsigned int *dstPitch = (unsigned int *)dstPitches;
   const dim_t *dstPitch = outT->strides().data();
-  // unsigned int *actPitch = (unsigned int *)srcPitches;
   const dim_t *actPitch = inT->strides().data();
-  
-  unsigned int typeSize = getsize<srcType>();
-  unsigned int numElemsDst =
-      dstPitch[0] * actIndex[0]; // Total number of elements in the tensor
 
-  // We give to each minion an initial address the number of positions that it
-  // must work on (maxRead).
+  // Total number of elements in the tensor
+  unsigned int numElemsDst = dstPitch[0] * dstIndex[0]; 
+
+  // We give to each minion an initial address and the number of positions that
+  // it must work on (maxRead).
   unsigned int initialAddr, maxRead;
+  size_t typeSize = getsize<srcType>();
   getCachelinePartition(typeSize, numElemsDst, initialAddr, maxRead,
                         minionId, activeMinions, dst);
   if (maxRead == 0)
     return;
+  
+
   // We move the initialAddr to the next non-padding position
   unsigned int srcDimNum = static_cast<unsigned int>(inT->ndims());
   
-  unsigned int k = 0;            // Amount of non-zero coordinates
+  unsigned int k;                // Amount of non-zero coordinates
   unsigned int coord[srcDimNum]; // Vector of coordinates
-  for (size_t index = 0; index < srcDimNum; ++index) {
-    coord[index] = 0;
-  }
-  
+
   /*use overloading WIP sw2400 sw2429*/
   getNonPaddingCoordinates(coord, initialAddr, srcDimNum, dstPitch, actIndex, k);
 
@@ -283,132 +254,30 @@ inline void fwdLibCopyInst(LibTensor* outT, LibTensor* inT,
   }
 
   unsigned int posMax = maxRead + initialAddr;
+  // In each iteration we copy a full inner dimension and switch to the next one,
+  // until completion.
   bool done = false;
-  unsigned int lastDim = srcDimNum - 1;
+  while (!done && (offsetOut < posMax)) {
+    // Figure out how many elements pending to be copied in current position
+    // Check that it is not copying out of bounds
+    size_t elemsToCopy = dstIndex[srcDimNum - 1] - coord[srcDimNum - 1];
+    if ((offsetOut + elemsToCopy) > posMax) { elemsToCopy = posMax - offsetOut; }
 
-  unsigned int laneElems = 4 / typeSize;
-  unsigned int registerElems = 0;
-  if (laneElems != 0) {
-    registerElems = 8 * laneElems;
-  } else {
-    registerElems = 4;
+    // Copies all the bytes pending
+    copyBytes(&src[offsetIn * typeSize], &dst[offsetOut * typeSize], elemsToCopy * typeSize);
+
+    // Updates pointers
+    if (coord[srcDimNum - 1] != 0) {
+      // Aligning the highest dimension is only required in the first iteration
+      // We move offsets to the begining of the second to last dimension
+      offsetIn  -= coord[srcDimNum - 1] * actPitch[srcDimNum - 1];
+      offsetOut -= coord[srcDimNum - 1] * dstPitch[srcDimNum - 1];
+      coord[srcDimNum - 1] = 0;
+    }
+    // Increment pointers ignoring the highest dimension as each step takes care
+    // of it
+    done = getOffsets(srcDimNum - 1, coord, offsetIn, offsetOut, actIndex, actPitch, dstPitch);
   }
-  unsigned int maxRow = (srcDimNum > 1) ? posMax / dstPitch[lastDim - 1] : 0;
-  unsigned int elementsInRow = 0, registersInRow = 0, res = 0, spareElems = 0, fullLanes = 0;
-  uint8_t mask = 0;
-  bool firstRow = true;
-  bool midRow = false;
-  bool lastRow = false;
-
-  if (srcDimNum == 1) {
-    lastDim++;
-    coord[0] = 0;
-  }
-
-  while ((offsetOut < posMax) && !done) {
-    if (lastDim != 0) {
-      if (firstRow && (coord[lastDim - 1] != maxRow)) {
-        elementsInRow = dstIndex[lastDim] - coord[lastDim];
-      } else if (coord[lastDim - 1] == maxRow) {
-        lastRow = true;
-        elementsInRow = posMax - offsetOut;
-      }
-      else {
-      elementsInRow = dstIndex[lastDim];
-      }
-    } else {
-      elementsInRow = dstIndex[lastDim];
-    }
-    if (firstRow || lastRow || !midRow) { // cases where variable update is needed.
-      registersInRow = elementsInRow / registerElems;
-      res = elementsInRow - registersInRow * registerElems;
-      if (laneElems != 0) {
-        fullLanes = res / laneElems;
-        spareElems = res - fullLanes * laneElems;
-      } else {
-        fullLanes = res * 2;
-        spareElems = 0;
-      }
-      mask = ((1 << fullLanes) - 1);
-      __asm__ __volatile__("mov.m.x m0, %[mask], 0 \n" : : [ mask ] "r"(mask) :);
-      if (!firstRow) midRow = true;
-    }
-    firstRow = false;
-    src8 += offsetIn * typeSize;
-    dst8 += offsetOut * typeSize;
-
-#ifndef DNN_LIB_DO_NOT_UNROLL_LOOPS
-    uint8_t *endPtrUnrolled = dst8 + (registersInRow & (~(0x20*8 -1)) ) * 0x20; // unrolling 8 stores in the same iteration
-    for(; dst8 < endPtrUnrolled; dst8+=0x20*8, src8+=0x20*8){ // copying using 8 registers at a time
-      float scratch[8];
-      __asm__ __volatile__
-        (
-         "flq2 %[d0], 0x20*0 + %[src] \n"
-         "flq2 %[d1], 0x20*1 + %[src] \n"
-         "flq2 %[d2], 0x20*2 + %[src] \n"
-         "flq2 %[d3], 0x20*3 + %[src] \n"
-         "flq2 %[d4], 0x20*4 + %[src] \n"
-         "flq2 %[d5], 0x20*5 + %[src] \n"
-         "flq2 %[d6], 0x20*6 + %[src] \n"
-         "flq2 %[d7], 0x20*7 + %[src] \n"
-         
-         "fsq2 %[d0], 0x20*0 + %[dst] \n"
-         "fsq2 %[d1], 0x20*1 + %[dst] \n"
-         "fsq2 %[d2], 0x20*2 + %[dst] \n"
-         "fsq2 %[d3], 0x20*3 + %[dst] \n"
-         "fsq2 %[d4], 0x20*4 + %[dst] \n"
-         "fsq2 %[d5], 0x20*5 + %[dst] \n"
-         "fsq2 %[d6], 0x20*6 + %[dst] \n"
-         "fsq2 %[d7], 0x20*7 + %[dst] \n"
-
-         : [dst] "=m" (* (uint8_t(*)[32*8]) dst8),
-           [d0] "=&f" (scratch[0]), [d1] "=&f" (scratch[1]), [d2] "=&f" (scratch[2]), [d3] "=&f" (scratch[3]),
-           [d4] "=&f" (scratch[4]), [d5] "=&f" (scratch[5]), [d6] "=&f" (scratch[6]), [d7] "=&f" (scratch[7])
-         : [src] "m" (* (const uint8_t(*)[32*8]) src8)
-         );
-    }
-#endif
-    uint8_t *endPtr = dst8 + registersInRow * 0x20;
-    for(; dst8 < endPtr; dst8+=0x20, src8+=0x20){ // copying using 8 registers at a time
-      float scratch;
-      __asm__ __volatile__
-        (
-         "flq2 %[d], %[src] \n"
-         "fsq2 %[d], %[dst] \n"
-         : [dst] "=m" (* ( uint8_t(*)[32]) dst8),
-           [d] "=&f" (scratch)
-         : [src] "m" (* (const uint8_t(*)[32]) src8)
-         );
-    }
-    float scratch;
-    __asm__ __volatile__("flw.ps %[d], %[src] \n"
-                         "fsw.ps %[d], %[dst] \n"
-                         : [dst] "=m" (* ( uint8_t(*)[32]) dst8),
-                           [d] "=&f" (scratch)
-                         : [src] "m" (* (const uint8_t(*)[32]) src8)
-                         );
-    src8 += fullLanes * 4;
-    dst8 += fullLanes * 4;
-    unsigned int offsetInAux = (src8 - src8Init) / typeSize;
-    unsigned int offsetOutAux = (dst8 - dst8Init) / typeSize;
-    for (unsigned int i = 0; i < spareElems; i++) {
-      tOutput[offsetOutAux + i] = tInput[offsetInAux + i];
-    }
-
-    if (lastRow)
-      break;
-
-    src8 = src8Init;
-    dst8 = dst8Init;
-    offsetIn -= coord[lastDim] * actPitch[lastDim];
-    offsetOut -= coord[lastDim] * dstPitch[lastDim];
-    coord[lastDim] = 0;
-
-    /*use overloading WIP sw2400 sw2429*/    
-    done = getOffsets(srcDimNum - 1, coord, offsetIn, offsetOut, actIndex,
-                      actPitch, dstPitch);
-  }
-  __asm__ __volatile__ ("mov.m.x m0, zero, 0xff \n");
   if (!DO_EVICTS)
     return;
   unsigned int clperminion = maxRead * typeSize / CACHE_LINE_BYTES;
