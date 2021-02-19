abb7a4f32ed41418027a7bd92160657340741ca1
Abel Bernabeu <abel.bernabeu@esperantotech.com>
2021-02-19 15:23:02 +0100

    [dnn-library] [SW-5846] Reworked the vectorized implementation of LRN
    The vectorized LocalRensponseNormalization implementation had several
    issues:
    
    - Did not support any kind of padding for the source operand due to a
      bug.
    - Even after fixing this bug, it only worked for FloatTy.
    - It was difficult to extend to new formats.
    
    I have reworked the code for addressing those issues and added support
    for Float16Ty and BFloatTy. Now that the vectorized code supports all
    the possible formats I have made it the default implementation returned
    by the implementation selector.

--- dnn-library/include/LibApiImplSel.h
+++ dnn-library/include/LibApiImplSel.h
@@ -190,10 +190,7 @@ namespace dnn_lib {
     //   0: base implementation (threaded)
     //   1: Vectorized 
     static size_t LocalResponseNormalization(std::vector<LibTensor*> &outTensors, std::vector<LibTensor*> &inTensors){
-      ElemKind elK = inTensors[0]->getElementType();
-      if (elK != FloatTy) return 0; // vectorized version just supports float
-      else return 1;
-      return 0;
+      return 1;
     }
 
     
