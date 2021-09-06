32823925b35bc271b786a6a71eb13667c2d20323
Pau Farre <pau.farre@esperantotech.com>
2021-09-06 15:06:06 +0200

    [dnn-library-api] Add required LibTypes.h

---
+++ dnn-library-api/include/dnnLibraryApi/LibTypes.h
@@ -0,0 +1,140 @@
+/*-------------------------------------------------------------------------
+ * Copyright (C) 2019, Esperanto Technologies Inc.
+ * The copyright to the computer program(s) herein is the
+ * property of Esperanto Technologies, Inc. All Rights Reserved.
+ * The program(s) may be used and/or copied only with
+ * the written permission of Esperanto Technologies and
+ * in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef LIB_TYPES_H
+#define LIB_TYPES_H
+
+#include <stdint.h>
+#include <array>
+
+#define INLINE_ATTR __attribute__((always_inline)) inline
+
+#define CACHE_LINE_BYTES 64
+#define MIN_PER_SHIRE 32
+
+//#define DIM_T_32
+namespace dnn_lib {
+
+#ifdef DIM_T_32
+using dim_t = uint32_t;
+using sdim_t = int32_t;
+#else
+using dim_t = size_t;
+using sdim_t = int64_t;
+#endif
+ 
+/// An enum representing the type used by the elements of a tensor. The types of
+/// Handles for these tensors should match the element kind.
+enum ElemKind : unsigned char {
+  // 32-bit float type (float)
+  FloatTy,
+  // 16-bit float type (half, fp16)
+  Float16Ty,
+  // 16-bit float type (bfloat16)
+  BFloat16Ty,
+  // 8-bit quantized type (int8_t)
+  Int8QTy,
+  // unsigned 8-bit quantized type (uint8_t)
+  UInt8QTy,
+  // 16-bit quantized type (int16_t)
+  Int16QTy,
+  // 32-bit quantized type (int32_t)
+  Int32QTy,
+  // 32-bit index type (int32_t)
+  Int32ITy,
+  // 64-bit index type (int64_t)
+  Int64ITy,
+  // 8-bit quantized type with fused scale/offset (uint8_t)
+  UInt8FusedQTy,
+  // 8-bit quantized type with fused FP16 scale/offset (uint8_t)
+  UInt8FusedFP16QTy,
+  // 4-bit quantized type with fused FP16 scale/offset (uint8_t, each byte
+  // represents 2 4-bit quantized data)
+  UInt4FusedFP16QTy,
+  // 4-bit quantized type with fused FP32 scale/offset (uint8_t, each byte
+  // represents 2 4-bit quantized data)
+  UInt4FusedQTy,
+  // Bool type (bool)
+  BoolTy,
+};
+
+// enum class PrecisionMode {
+//  //TODO: Get same enumerate as Jitter
+//  PM_FP_32   = 0,   // fp32
+//  PM_FP_16   = 1,   // fp16
+//  PM_BFP_16  = 2,   // bfloat16
+//  PM_INT_32  = 3,   // quant int32
+//  PM_INT_8   = 4,   // quant int8
+//  PM_INT_16  = 5,   // quant int16
+//  PM_INT_I32 = 6,   // idx int32
+//  PM_INT_I64 = 7,   // idx int64
+//  PM_UINT_8  = 8,   // quant uint8
+//  PM_UINT_4  = 9,   // quant uint4
+//  PM_BOOL    = 10,  // bool
+//  MAX_PRECISION_MODES
+// };
+
+// template<class T>
+// constexpr std::size_t getsize() {
+//   return sizeof(T);
+// }
+// template<>
+// constexpr std::size_t getsize<float16>() {
+//   return 2;
+// }
+
+  template <bool>
+  struct conditional_;
+  template<>
+  struct conditional_<false> {
+    template <typename, typename T>
+    using apply = T;
+  };
+
+  template <bool V, typename T, typename F>
+  using conditional_t = typename conditional_<V>::template apply<T, F>;
+  
+  /*@brief returns is \p elk is a quantized ElemKind.
+   */
+  inline constexpr bool isQuantizedElemKind(dnn_lib::ElemKind elk) {
+    if (elk == dnn_lib::ElemKind::Int8QTy ||
+        elk == dnn_lib::ElemKind::UInt8QTy ||
+        elk == dnn_lib::ElemKind::Int16QTy ||
+        elk == dnn_lib::ElemKind::Int32QTy ||
+        elk == dnn_lib::ElemKind::UInt8FusedQTy ||
+        elk == dnn_lib::ElemKind::UInt8FusedFP16QTy ||
+        elk == dnn_lib::ElemKind::UInt4FusedFP16QTy)
+      return true;
+    else
+      return false;
+  }
+
+  /*@brief returns whether \p elk is an "index" ElemKind.
+   */
+  inline constexpr bool isIndexElemKind(dnn_lib::ElemKind elk) {
+    return elk == dnn_lib::ElemKind::Int32ITy or elk == dnn_lib::ElemKind::Int64ITy;
+  }
+
+  /*@brief returns wheter \p elk is a fused quantized ElemKind.
+   */
+  inline bool isFusedQuantizedElemKind(ElemKind elk) {
+    if (elk == dnn_lib::ElemKind::UInt8FusedQTy ||
+        elk == dnn_lib::ElemKind::UInt8FusedFP16QTy ||
+        elk == dnn_lib::ElemKind::UInt4FusedFP16QTy)
+      return true;
+    else
+      return false;
+  }
+
+
+  
+}
+#endif //LIB_TYPES_H
