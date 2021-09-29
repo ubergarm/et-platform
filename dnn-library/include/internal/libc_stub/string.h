0d43d7ce3b51d69b9cf6143036d32be99f209638
Pau Farre <pau.farre@esperantotech.com>
2021-09-29 10:48:48 +0000

    [dnn-library] [SW-9279] Add cplusplus preprocessor guards to make sure we get C linkage

--- dnn-library/include/internal/libc_stub/string.h
+++ dnn-library/include/internal/libc_stub/string.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 size_t et_strlen(const char *str);
 
+#ifdef __cplusplus
+}
+#endif
