dee56010e092f0d624cf344e143493e2c47b9be0
Nivard Aymerich <nivard.aymerich@esperantotech.com>
2021-10-25 21:44:56 +0200

    [dnn-library-api] [SW-9838] No instruction is using global stores yet - fix instrConfig report accordingly

--- dnn-library-api/src/Api.cpp
+++ dnn-library-api/src/Api.cpp
@@ -864,7 +864,7 @@ void initInstrConfigTable() {
     {{{operandState::untouched, operandState::untouched, operandState::untouched, operandState::untouched,
        operandState::untouched}}},
     {0x0}, // evict available mask
-    {0x1}  // global store mask
+    {0x0}  // global store mask
   };
   instrConfigTable.push_back(instConfigInt);
 
@@ -1025,7 +1025,7 @@ void initInstrConfigTable() {
     {{{operandState::untouched, operandState::untouched, operandState::untouched, operandState::untouched,
        operandState::untouched}}},
     {0x0}, // evict available mask
-    {0x1}  // global store mask
+    {0x0}  // global store mask
   };
   instrConfigTable.push_back(instConfigInt);
 
@@ -1045,7 +1045,7 @@ void initInstrConfigTable() {
     // CB states per impl
     {{{operandState::untouched, operandState::untouched, operandState::untouched, operandState::untouched}}},
     {0x0}, // evict available mask
-    {0x1}  // global store mask
+    {0x0}  // global store mask
   };
   instrConfigTable.push_back(instConfigInt);
 
