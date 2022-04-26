5901a023ef39c04356984e63971dd9b3fb0fe071
Constantino Gomez <constantino.gomez@esperantotech.com>
2022-04-26 09:03:39 +0000

    [dnn-library-api] [SW-11916] Enable SparseLengthSum multiop threaded version

--- dnn-library-api/src/ApiImplSel.cpp
+++ dnn-library-api/src/ApiImplSel.cpp
@@ -134,7 +134,7 @@ size_t implSel::LocalResponseNormalization(std::vector<LibTensor*>& outTensors, 
 
 size_t implSel::SparseLengthsSum([[maybe_unused]] std::vector<LibTensor*>& outTensors,
                                  [[maybe_unused]] std::vector<LibTensor*>& inTensors) {
-  return 0;
+  return 1;
 }
 
 size_t implSel::SparseLengthsWeightedSum(std::vector<LibTensor*>& outTensors, std::vector<LibTensor*>& inTensors) {
