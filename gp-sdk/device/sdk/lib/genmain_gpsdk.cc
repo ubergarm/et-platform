97f28d6eebe0ba0e2007bb9055c8159d9b5476a1
rafa <rafael.jimenez@esperantotech.com>
2022-11-04 16:27:03 +0100

    [gp-sdk] [SW-14387][SW-14388][SW-14389] Clean up uberkernel and build structure for tests.

---
+++ gp-sdk/device/sdk/lib/genmain_gpsdk.cc
@@ -0,0 +1,98 @@
+// clang-format off
+
+
+#include <stdio.h>
+
+#include <etsoc/isa/hart.h>
+#include <etsoc/isa/barriers.h>
+#include <etsoc/isa/cacheops-umode.h>
+#include <etsoc/common/utils.h>
+#include "SyncComputeNode.h"
+#include "kernel_arguments.h"
+#include "usermain.h"
+#include "inst_pref_decls.h"
+
+extern "C" void startMainGpSdk(uint64_t shireId, uint32_t minionId, kernelArguments * layer_dyn_info);
+
+extern "C" int main_gpsdk(kernelArguments * layer_dyn_info) {
+  uint32_t hart = get_hart_id();
+  uint32_t threadId = hart & 1;
+  uint32_t minionId = hart >> 1;
+  uint32_t shireId = minionId >> 5;
+  minionId = minionId & 0x1F;
+
+  startMainGpSdk(shireId, minionId, layer_dyn_info);
+  return 0;
+
+}
+
+
+#define _UNIQUE_CALL(function, ...)\
+ do { \
+  function( __VA_ARGS__ ); \
+  __asm__ __volatile__ (\
+    ".global " #function "_return_point\n"\
+    #function "_return_point:" : : : );\
+} while(0);
+
+
+#define _UNIQUE_CALL_PARALLEL(function, param) {\
+  function( param ); \
+}
+
+
+#define _UNIQUE_CALL_PARALLEL_RETURN(function_name) {\
+  __asm__ __volatile__ (\
+    ".global " #function_name "_return_point\n"\
+    #function_name "_return_point:" : : : );\
+}
+
+
+#define _UNIQUE_LBL(function, param) {\
+  __asm__ __volatile__ (\
+    ".global " #function "_return_point\n"\
+    #function "_return_point:" : : : );\
+}
+
+extern "C" __attribute((noclone , noinline)) void startMainGpSdk(uint64_t shireId, uint32_t minionId, kernelArguments * layer_dyn_info){
+
+  uint32_t globalMinionId = shireId * 32 + minionId;
+
+  _UNIQUE_CALL(usermain, layer_dyn_info)
+
+    //SyncComputeNode(minionId, shireId, 32, false, true, 0);
+
+}
+
+
+extern "C" __attribute((noclone , noinline)) void SyncMinionsCode(uint32_t minionId, uint32_t threadId, uint32_t nComputeShires, uint32_t syncThread0Mask, uint32_t syncThread1Mask, kernelArguments * layer_dyn_info) {
+  if (threadId & 0x1) {
+    if (((syncThread1Mask >> minionId) & 0x1) == 0) return;
+  } else {
+    if (((syncThread0Mask >> minionId) & 0x1) == 0) return;
+  }
+
+
+  //
+  // Sync for node testOperator_Sync_5
+  //
+
+  global_barrier_receiver(
+      FCC_0,                    // Which FCC to wait for
+      0,                        // FLB to be used in the source shire for the barrier
+      minionId,                 // Sync minion id
+      threadId,                 // Sync thread id
+      THREAD_1,                 // Thread of the FCC dest
+      FCC_1,                    // FCC for dest
+      HELPER_ACTIVATION_THREADS // Mask of minions in dest shire to receive FCC
+    | HELPER_WEIGHTS_THREADS
+    | HELPER_CODE_THREADS
+    | HELPER_EVICT_W_THREADS
+    ,nComputeShires
+    ,syncThread0Mask //mask of threads 0 of the sync Minions that will receive the credit
+    ,syncThread1Mask //mask of threads 1 of the sync Minions that will receive the credit
+    ,32
+    ,0
+    ,0
+    );
+}
---
+++ gp-sdk/device/tests/print2/usermain.cc
@@ -0,0 +1,34 @@
+// clang-format off
+
+
+#include <array>
+#include <stdio.h>
+//#include <device_common.h>
+#include <etsoc/common/utils.h>
+#include <etsoc/isa/fcc.h>
+#include <etsoc/isa/hart.h>
+#include <etsoc/isa/tensors.h>
+#include <etsoc/isa/utils.h>
+
+#include "SyncComputeNode.h"
+#include "inst_pref_decls.h"
+
+#include "usermain.h"
+
+#include "kernel_arguments.h"
+
+
+typedef int8_t i8;
+
+
+__attribute__((noinline))
+int usermain(kernelArguments * layer_dyn_info) {
+  //    fcc(FCC_0);
+
+
+    if (get_minion_id()==0) {
+      et_printf("%s,%d HELLO WORLD!!!!\n",__func__,__LINE__);
+      et_printf("%s,%d PRINT2 test executed!!!!\n",__func__,__LINE__);      
+    }
+    return 0;
+}
