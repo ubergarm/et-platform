8c047c16282ee418a105fc6e4aa33415122be221
rafa <rafael.jimenez@esperantotech.com>
2022-11-09 17:57:36 +0100

    [gp-sdk] Changes variables name to avoid confusion.

--- gp-sdk/device/tests/tests.cmake
+++ gp-sdk/device/tests/tests.cmake
@@ -22,6 +22,7 @@ macro(add_etsoc_riscv_executable TARGET_NAME TARGET_SOURCES)
     et-common-libs::cm-umode
   )
 
+  #@TODO we should commonoalize the options into some flags set to avoid repeating them.
   set(ELF_EXE_LINKER_FLAGS "-nostdlib -nostartfiles -Wl,--gc-sections -Xlinker -Map=${MAP_FILE} -e _start -Wl,--start-group -lm -lgcc -T ${LINKER_SCRIPT_ABS_PATH}")
   
   set_target_properties(${TARGET_NAME}
@@ -38,37 +39,37 @@ macro(add_etsoc_riscv_executable TARGET_NAME TARGET_SOURCES)
     "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/${MAP_FILE}"
   )
 
-  set(LINKER_SCRIPT_ABS_PATH  ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/relinker.ld)
-  set(MAP_FILE "${TARGET_NAME}_dbg.map")
-  set(ELF_EXE_LINKER_FLAGS "-nostdlib -nostartfiles -Wl,--gc-sections -Xlinker -Map=${MAP_FILE} -e _start -Wl,--start-group -lm -lgcc -T ${LINKER_SCRIPT_ABS_PATH}")
-  set(RELINK_TARGET "${TARGET_NAME}_dbg")
+  set(LINKER_SCRIPT_ABS_PATH  ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/debug_linker.ld)
+  set(DEBUG_MAP_FILE "${TARGET_NAME}_dbg.map")
+  set(ELF_EXE_LINKER_FLAGS "-nostdlib -nostartfiles -Wl,--gc-sections -Xlinker -Map=${DEBUG_MAP_FILE} -e _start -Wl,--start-group -lm -lgcc -T ${LINKER_SCRIPT_ABS_PATH}")
+  set(DEBUG_TARGET "${TARGET_NAME}_dbg")
   
-  add_executable(${RELINK_TARGET} ${TARGET_SOURCES})
+  add_executable(${DEBUG_TARGET} ${TARGET_SOURCES})
 
-  target_link_libraries(${RELINK_TARGET}
+  target_link_libraries(${DEBUG_TARGET}
     PRIVATE
     crt_shared
     et-common-libs::cm-umode
   )
 
-  set_target_properties(${RELINK_TARGET}
+  set_target_properties(${DEBUG_TARGET}
     PROPERTIES
     LINK_DEPENDS ${LINKER_SCRIPT_ABS_PATH}
     LINK_FLAGS ${ELF_EXE_LINKER_FLAGS}
     RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}" #bin
   )
 
-  add_custom_target("GenLinker_${RELINK_TARGET}"
-    COMMAND sed '0,/0x00000001/s//${ADDRESS}/' ${PROJECT_SOURCE_DIR}/sdk/lib/linker.ld > ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/relinker.ld    
+  add_custom_target("GenLinker_${DEBUG_TARGET}"
+    COMMAND sed '0,/0x00000001/s//${ADDRESS}/' ${PROJECT_SOURCE_DIR}/sdk/lib/linker.ld > ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/debug_linker.ld    
   )
 
-  add_dependencies(${RELINK_TARGET} "GenLinker_${RELINK_TARGET}")
+  add_dependencies(${DEBUG_TARGET} "GenLinker_${DEBUG_TARGET}")
 
-  add_custom_command(TARGET ${RELINK_TARGET}
+  add_custom_command(TARGET ${DEBUG_TARGET}
     POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E rename "${CMAKE_CURRENT_BINARY_DIR}/${MAP_FILE}"
-    "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/${MAP_FILE}"    
-    COMMAND ${CMAKE_COMMAND} -E rm "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/relinker.ld"
+    COMMAND ${CMAKE_COMMAND} -E rename "${CMAKE_CURRENT_BINARY_DIR}/${DEBUG_MAP_FILE}"
+    "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/${DEBUG_MAP_FILE}"    
+    COMMAND ${CMAKE_COMMAND} -E rm "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}/debug_linker.ld"
   )
   
 endmacro(add_etsoc_riscv_executable)
