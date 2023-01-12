b30ec520316d5c80f74255e41c1d8e6459a7a1c6
Calvin Bulla <calvin.bulla@esperantotech.com>
2023-01-11 16:44:02 -0800

    [gp-sdk] [CS-49] Single linker script for device kernels
    We can use `--defsym` to link with a same single linker script,
    but targetting two different kernel base addresses.
    
    This means we no longer need to generate a debug linker script
    .. which in a way fixes the issue in CS-49 :smirk_cat:
    
    Other changes:
    - Edit comments in cmake script

--- gp-sdk/device/riscv_helpers.cmake
+++ gp-sdk/device/riscv_helpers.cmake
@@ -8,39 +8,40 @@
 # agreement/contract under which the program(s) have been supplied.
 #------------------------------------------------------------------------------
 
+math(EXPR DEBUG_ADDRESS "${ADDRESS} + 0x1000" OUTPUT_FORMAT HEXADECIMAL)
+message(STATUS "Base address used to relink (debug) ELFs -> ${DEBUG_ADDRESS}")
+
 #
-# Adds a riscv executable prepared for being executed as a kernel on ET-SoC-1 
+# Adds a riscv executable prepared for being executed as a kernel on ET-SoC-1.
 # @param TARGET_NAME name of the cmake target to be created.
 # @param TARGET_SOURCES_LIST space-separated list of source files
 # Notes:
-# - a secondary ${TARGET}_dbg is created (linked to a runtime-predicted address to be used
-#   with debuggers
-# - the created target is a standard cmake target, so compilation options and dependencies 
-#   can be completed by cmake standard calls (target_link_libraries, target_include_directories,...)
+# - This will also create a secondary target `${TARGET_NAME}_dbg`.
+#   The debug target is relinked using a runtime-predicted address and should be used with debuggers.
+# - The created target is a standard cmake target, so compilation options and dependencies  can be
+#   set with standard cmake functions (target_link_libraries, target_include_directories, etc.).
 #
 macro(add_etsoc_riscv_executable TARGET_NAME TARGET_SOURCES_LIST) 
 
-  #merge param list
+  # merge parameter list
   set(TARGET_SOURCES ${TARGET_SOURCES_LIST} ${ARGN} ) 
 
   set(LINKER_SCRIPT_ABS_PATH ${PROJECT_SOURCE_DIR}/sdk/lib/linker.ld)
-  set(LINKER_SCRIPT_ABS_PATH_DBG  ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}_debug_linker.ld)
 
   add_executable(${TARGET_NAME} ${TARGET_SOURCES})
   
-  #enabling exports allows crating a dependeny with the host app to 
-  #reach xxx_kernel_arguments header exported by the kernel
+  # enabling exports allows crating a dependeny with the host app to 
+  # reach xxx_kernel_arguments header exported by the kernel
   set_property(TARGET ${TARGET_NAME} PROPERTY ENABLE_EXPORTS 1)
  
   target_link_libraries(${TARGET_NAME}
     et-common-libs::cm-umode
   )
-  
+
   set(ELF_EXE_LINKER_FLAGS_BASE "-nostdlib -nostartfiles -Wl,--gc-sections  -e _start -Wl,--start-group -lm -lgcc")
   
-  set(ELF_EXE_LINKER_FLAGS "${ELF_EXE_LINKER_FLAGS_BASE} -T ${LINKER_SCRIPT_ABS_PATH}")
-  set(ELF_EXE_LINKER_FLAGS_DBG "${ELF_EXE_LINKER_FLAGS_BASE} -T ${LINKER_SCRIPT_ABS_PATH_DBG}")
-
+  set(ELF_EXE_LINKER_FLAGS "${ELF_EXE_LINKER_FLAGS_BASE} -T ${LINKER_SCRIPT_ABS_PATH} -Wl,--defsym=BASE_ADDRESS=0")
+  set(ELF_EXE_LINKER_FLAGS_DBG "${ELF_EXE_LINKER_FLAGS_BASE} -T ${LINKER_SCRIPT_ABS_PATH} -Wl,--defsym=BASE_ADDRESS=${DEBUG_ADDRESS}")
 
   target_compile_options(${TARGET_NAME} PRIVATE -fno-exceptions -falign-functions=64 -O3 -g3 -Wstack-usage=4096 -DNDEBUG)
   target_compile_definitions(${TARGET_NAME} PRIVATE "-DNDEBUG")
@@ -51,7 +52,7 @@ macro(add_etsoc_riscv_executable TARGET_NAME TARGET_SOURCES_LIST)
     LINK_FLAGS ${ELF_EXE_LINKER_FLAGS}
   )
   
-  # adding a second "debug" target (identical, but linked using a different linker script)
+  # adding a second "debug" target (identical, but linked using a different base address)
   set(DEBUG_TARGET "${TARGET_NAME}_dbg")
   
   add_executable(${DEBUG_TARGET} $<TARGET_PROPERTY:${TARGET_NAME},SOURCES>)
@@ -63,23 +64,8 @@ macro(add_etsoc_riscv_executable TARGET_NAME TARGET_SOURCES_LIST)
 
   set_target_properties(${DEBUG_TARGET}  
      PROPERTIES  
-     LINK_DEPENDS ${LINKER_SCRIPT_ABS_PATH_DBG}
+     LINK_DEPENDS ${LINKER_SCRIPT_ABS_PATH}
      LINK_FLAGS  ${ELF_EXE_LINKER_FLAGS_DBG})
-
-  math(EXPR DEBUG_ADDRESS "${ADDRESS} + 0x1000" OUTPUT_FORMAT HEXADECIMAL)
-  message(STATUS "base address used to produce relinked (debug) elfs-> ${DEBUG_ADDRESS}")
-
-  #NOTE: keep track of this placeholder on original linker-script)
-  add_custom_target("GenLinker_${DEBUG_TARGET}"
-    COMMAND sed '0,/0x00000000\; \\/\\* BASE_ADDRESS_PLACEHOLDER \\*\\//s//${DEBUG_ADDRESS}\; \\/\\* BASE_ADDRESS_PLACEHOLDER \\*\\//' ${PROJECT_SOURCE_DIR}/sdk/lib/linker.ld > ${LINKER_SCRIPT_ABS_PATH_DBG}   
-  )
-  
-  add_dependencies(${DEBUG_TARGET} "GenLinker_${DEBUG_TARGET}")
-
-  add_custom_command(TARGET ${DEBUG_TARGET}
-    POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E rm  ${LINKER_SCRIPT_ABS_PATH_DBG}
-  )
 
   add_custom_command(TARGET ${DEBUG_TARGET}
     POST_BUILD
