9e047a1742d9500ede1872178c0c53a8f6c76464
Pere Munt <pere.munt@esperantotech.com>
2023-02-03 15:25:07 +0100

    [gp-sdk] [SW-15600] added __throw__XXX stubs. - Trick is to enable stl code (i.e std::bitset) to compile even w/o using libstdc++ to resolve potential throw/abort decisions

---
+++ gp-sdk/device/sdk/libcpp_throw_stub/functexcept.cc
@@ -0,0 +1,117 @@
+/*-------------------------------------------------------------------------
+ * Copyright (C) 2023, Esperanto Technologies Inc.
+ * The copyright to the computer program(s) herein is the
+ * property of Esperanto Technologies, Inc. All Rights Reserved.
+ * The program(s) may be used and/or copied only with
+ * the written permission of Esperanto Technologies and
+ * in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ *-------------------------------------------------------------------------
+ */
+
+#include <etsoc/common/utils.h>
+
+/* stub implementations of __throw__XXX libstdc++ functions.
+ * as we are working with -nostdlib -fno-exceptions.. we need a way for the stdlib headers
+ * to abort. (This is tipically delegated to libsdc++ and it decides based on having exceptions
+ * enabled or not).
+ *
+ * based on https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/src/c%2B%2B11/functexcept.cc
+ */
+
+namespace std {
+// declarations
+//
+void __throw_bad_exception();
+void __throw_bad_alloc();
+void __throw_bad_array_new_length();
+void __throw_bad_cast();
+void __throw_bad_typeid();
+void __throw_logic_error(const char* __s __attribute__((unused)));
+void __throw_domain_error(const char* __s __attribute__((unused)));
+void __throw_invalid_argument(const char* __s __attribute__((unused)));
+void __throw_length_error(const char* __s __attribute__((unused)));
+void __throw_out_of_range(const char* __s __attribute__((unused)));
+void __throw_out_of_range_fmt(const char* __fmt, ...);
+void __throw_runtime_error(const char* __s __attribute__((unused)));
+void __throw_range_error(const char* __s __attribute__((unused)));
+void __throw_overflow_error(const char* __s __attribute__((unused)));
+void __throw_underflow_error(const char* __s __attribute__((unused)));
+
+// definitions
+
+void __attribute__((weak)) __throw_bad_exception() {
+  et_assert(false);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_bad_alloc() {
+  et_assert(false);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_bad_array_new_length() {
+  et_assert(false);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_bad_cast() {
+  et_assert(false);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_bad_typeid() {
+  et_assert(false);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_logic_error(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_domain_error(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_invalid_argument(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_length_error(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_out_of_range(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_out_of_range_fmt(const char* __fmt, ...) {
+  __throw_out_of_range(__fmt);
+}
+
+void __attribute__((weak)) __throw_runtime_error(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_range_error(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_overflow_error(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+void __attribute__((weak)) __throw_underflow_error(const char* __s __attribute__((unused))) {
+  et_assert(false && __s);
+  et_abort();
+}
+
+} // namespace std
