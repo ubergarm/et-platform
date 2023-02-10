a8a9820ecfa4219f0123c1de42743cf3dfaad008
Marc Lupon <marc.lupon@esperantotech.com>
2023-02-10 17:41:26 +0100

    [dnn-library-api] [Sw-16112] Keep only the scalar version for the CMPNEQ operator

--- dnn-library-api/src/ApiImplSel.cpp
+++ dnn-library-api/src/ApiImplSel.cpp
@@ -48,7 +48,9 @@ size_t implSel::ElementCmpEQ(std::vector<LibTensor*>& outTensors, std::vector<Li
 }
 
 size_t implSel::ElementCmpNEQ(std::vector<LibTensor*>& outTensors, std::vector<LibTensor*>& inTensors) {
-  return ElementBool(outTensors, inTensors);
+  // SW-16112: Add Vectorized implementation for CMPNEQ (currenty only works for FP16)
+  // return ElementBool(outTensors, inTensors);
+  return 0;
 }
 
 size_t implSel::ElementCmpLTE(std::vector<LibTensor*>& outTensors, std::vector<LibTensor*>& inTensors) {
