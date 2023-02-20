97488e3b709d409aaadc04ae3752940d8bac092a
rafa <rafael.jimenez@esperantotech.com>
2023-02-20 18:04:06 +0100

    [gp-sdk] [SW-15786] killing a runing app should not required resetting the card device.
    Clang applied.
    
    Rename ctrlCrelaunch test to what it does.
    
    Modify path and Cmake according to the new busy10sec name.

---
+++ gp-sdk/device/tests/busy10sec/busy10sec.cc
@@ -0,0 +1,37 @@
+/*-------------------------------------------------------------------------
+ * Copyright (C) 2023, Esperanto Technologies Inc.
+ * The copyright to the computer program(s) herein is the
+ * property of Esperanto Technologies, Inc. All Rights Reserved.
+ * The program(s) may be used and/or copied only with
+ * the written permission of Esperanto Technologies and
+ * in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ *-------------------------------------------------------------------------
+ */
+
+#include <etsoc/common/utils.h>
+#include <etsoc/isa/hart.h>
+
+#include "entryPoint.h"
+
+#define CLK_600MHZ_CYCLES_TO_SEC 1.6667E-09
+
+int entryPoint_0([[maybe_unused]] KernelArguments* args) {
+
+  auto minionId = get_minion_id();
+  uint64_t assignedMinions = 32;
+
+  if (minionId >= assignedMinions) {
+    return 0;
+  }
+  auto start = et_get_timestamp();
+  auto elapsed = et_get_delta_timestamp(start);
+
+  while ((elapsed * CLK_600MHZ_CYCLES_TO_SEC) < 10) {
+    elapsed = et_get_delta_timestamp(start);
+  }
+
+  et_printf("Success\n");
+
+  return 0;
+}
