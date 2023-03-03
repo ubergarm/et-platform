ec9425b04086eb9d2b7b9a20ce902af3427e1108
Hongmin Jin <hongmin.jin@esperantotech.com>
2023-03-03 15:48:55 +0000

    [dnn-library-api] [SW-16286] Migrate CI pipeline to use pre-release strategy
    - Migrate CI pipeline to use pre-release strategy described in https://esperantotech.atlassian.net/wiki/spaces/SW/pages/3013869574/How+to+update+projects+pipelines+to+enable+pipelines-for-tags+pre-release+package+generation

--- dnn-library-api/CHANGELOG.md
+++ dnn-library-api/CHANGELOG.md
@@ -14,6 +14,15 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 ### Security
 
+## [0.5.0]
+### Added
+### Changed
+- [SW-16286] Migrate CI pipeline to use pre-release strategy
+### Deprecated
+### Removed
+### Fixed
+### Security
+
 ## [0.4.0]
 ### Added
 ### Changed
--- dnn-library-api/conanfile.py
+++ dnn-library-api/conanfile.py
@@ -33,10 +33,11 @@ class DnnLibraryApiConan(ConanFile):
     generators = "CMakeDeps"
 
 
-    python_requires = "conan-common/[>=0.5.0 <1.0.0]"
+    python_requires = "conan-common/[>=1.1.0 <2.0.0]"
 
     def set_version(self):
-        self.version = self.python_requires["conan-common"].module.get_version_from_cmake_project(self, self.name)       
+        get_version = self.python_requires["conan-common"].module.get_version
+        self.version = get_version(self, self.name)
 
     def configure(self):
         if self.options.shared:
