524ec88ca576ff5b8cc2f09476e247e7ff35b6c1
Pere Munt <pere.munt@esperantotech.com>
2023-03-20 14:54:47 +0100

    [gp-sdk] [SW-16557] Tests to cover Instrumentation issues

---
+++ gp-sdk/device/tests/profiling/profiling_simple.cc
@@ -0,0 +1,37 @@
+/*-------------------------------------------------------------------------
+ * Copyright (C) 2023, Esperanto Technologies Inc.
+ * The copyright to the computer program(s) herein is the
+ * property of Esperanto Technologies, Inc. All Rights Reserved.
+ * The program(s) may be used and/or copied only with
+ * the written permission of Esperanto Technologies and
+ * in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ *-------------------------------------------------------------------------
+ */
+
+#include <etsoc/common/utils.h>
+#include <etsoc/isa/hart.h>
+#include <algorithm>
+
+#include "entryPoint.h"
+#include "profiling.h"
+
+
+int entryPoint_0(KernelArguments* vectors);
+extern  DeviceConfig config {1, entryPoint_0, nullptr};
+
+
+int entryPoint_0([[ maybe_unused ]] KernelArguments* vectors) {
+
+  if(get_hart_id() != 0) {
+   return 0;
+  }
+  //this test assumes ~4096 bytes per hart trace buffer.
+  //1000 is enough to stress the circular buffer wrap-around (4096 bytes) few times.
+  //goal is getting all the writes
+  constexpr size_t kMaxIters = 1000;
+  for(size_t i = 0; i < kMaxIters; i ++) {
+    SCOPED_USER_PROFILE_EVENT(1);
+  }
+  return 0;
+}
