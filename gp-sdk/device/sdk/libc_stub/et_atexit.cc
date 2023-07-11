86d973cb1f3f35f3184f2201c60d330c0160d308
Abel Bernabeu <abel.bernabeu@esperantotech.com>
2023-07-11 16:32:07 +0000

    [gp-sdk] Fixed cpp_contructor.elf build with clang
    The kernel needed a definition of atexit.

---
+++ gp-sdk/device/sdk/libc_stub/et_atexit.cc
@@ -0,0 +1,16 @@
+/*-------------------------------------------------------------------------
+ * Copyright (C) 2023, Esperanto Technologies Inc.
+ * The copyright to the computer program(s) herein is the
+ * property of Esperanto Technologies, Inc. All Rights Reserved.
+ * The program(s) may be used and/or copied only with
+ * the written permission of Esperanto Technologies and
+ * in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ * -------------------------------------------------------------------------
+ */
+
+extern "C" {
+int atexit(void (*function)(void)) {
+  return 0;
+}
+}
