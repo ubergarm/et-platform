9a3b2661a57d2ffea18f355435fe314605847c33
Pau Farre <pau.farre@esperantotech.com>
2023-11-08 16:29:38 +0000

    [dnn-library-api] [SW-19215] Generate gcc11 package

--- dnn-library-api/conan/ci_build.py
+++ dnn-library-api/conan/ci_build.py
@@ -11,6 +11,7 @@ def main():
     build.add_package(conanfile_path)
     build.add_consumer("ml/0.12.0@")
     build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-release")
+    build.add_configuration("default", "linux-ubuntu22.04-x86_64-gcc11-release")
     build.report()
 
     build.run()
