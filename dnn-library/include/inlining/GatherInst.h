4baa14f2dde9b0a21752c795f31a17df8c317cb1
Nivard Aymerich <nivard.aymerich@esperantotech.com>
2023-11-21 15:48:39 +0100

    [dnn-library] [SW-19415] Update Gather implementation to support negative indices - improve implementation to copy chunks of elements at last dimension instead of always copying one by one.

--- dnn-library/include/inlining/GatherInst.h
+++ dnn-library/include/inlining/GatherInst.h
@@ -12,7 +12,6 @@
 #ifndef _GATHER_INST_H_
 #define _GATHER_INST_H_
 
-#include "Addresser.h"
 #include "Float16.h"
 #include "LibTensor.h"
 #include "utils.h"
@@ -26,51 +25,39 @@ namespace dnn_lib {
 
 namespace inlining {
 
-
 // The threaded version of the GatherInst function generalises the function to
-// any given dimensions for the two source tensors (tInput and tIndices). tInput
-// dims: d0 x ··· x dn. batchedDims: i (number between 0 and n). tIndices dims:
-// i0 x ··· x ik. Then, the tOutput tensor dimensions are determined. tOutput
+// any given dimensions for the two source tensors (Src and Indices). Src
+// dims: d0 x ··· x dn. batchedDims: i (number between 0 and n). Indices dims:
+// i0 x ··· x ik. Then, the output tensor dimensions are determined. Dst
 // dims: d0 x ··· x d(i-1) x i0 x ··· x ik x d(i+1) x ··· x dn (dstDimsNum =
 // srcDimsNum + indicesDimsNum - 1). The GatherInst function consists in copying
 // the source tensor's elements in the following way:
-// tOutput(x0,...,x(i-1),y0,...,yk,x(i+1),...,xn) =
-// tInput(x0,...,x(i-1),tIndices(y0,...,yk),x(i+1),...,xn). The elements in the
-// tIndices tensor must be integers between 0 and di - 1, so they are valid
-// index values for the i-th dimension of the source tensor tInput.
+// Dst(x0,...,x(i-1),y0,...,yk,x(i+1),...,xn) =
+// Src(x0,...,x(i-1),Indices(y0,...,yk),x(i+1),...,xn). The elements in the
+// Indices tensor must be integers between -di and di - 1, so they are valid
+// index values for the i-th dimension of the source tensor Src.
 
 template <ElemKind srcElK, ElemKind indexElK>
 INLINE_ATTR void fwdLibGatherInst(LibTensor* outT, LibTensor* in1T, LibTensor* in2T, unsigned int batchedDims,
                                   uint64_t flags, const uint32_t minionOffset = 0, const uint32_t assignedMinions = 0) {
   using srcType = typename elemKind2elemTy<srcElK>::type;
-  //  using indexType = typename elemKind2elemTy<indexElK>::type;
+  using idxType = typename elemKind2elemTy<indexElK>::type;
 
   assert(get_minion_id() >= minionOffset);
+  assert((minionOffset == 0) or (minionOffset + assignedMinions < MIN_PER_SHIRE * activeShires(flags)));
   size_t minionId = get_minion_id() - minionOffset;
   size_t activeMinions = (assignedMinions == 0) ? (MIN_PER_SHIRE * activeShires(flags)) : assignedMinions;
   if (minionId >= activeMinions) return;
 
-  /* maintain compatibility through the new Iface Libtensor */
-  /* outT->dst  in1T--> src  in2T--> index*/
-  void* dstT = outT->getRawDataPointer();
-  void* srcT = in1T->getRawDataPointer();
-  void* indexT = in2T->getRawDataPointer();
+  auto dstT = outT->getRawDataPointer<uint8_t>();
+  auto srcT = in1T->getRawDataPointer<uint8_t>();
+  auto indexT = in2T->getRawDataPointer<idxType>();
 
-  Addresser<srcElK> tOutput(dstT, outT->getScale(), outT->getOffset());
-  const Addresser<srcElK> tInput(srcT, in1T->getScale(), in1T->getOffset());
-  const Addresser<indexElK> tIndices(indexT, in2T->getScale(), in2T->getOffset());
-  
-  // unsigned int *srcIndex = (unsigned int *)srcDims;
   const dim_t *srcIndex = in1T->dims().data();
-  // unsigned int *dstIndex = (unsigned int *)dstDims;
   const dim_t *dstIndex = outT->dims().data();
-  // unsigned int *indicesIndex = (unsigned int *)indicesDims;
   const dim_t *indicesIndex = in2T->dims().data();
-  // unsigned int *srcPitch = (unsigned int *)srcPitches;
   const dim_t *srcPitch = in1T->strides().data();
-  // unsigned int *dstPitch = (unsigned int *)dstPitches;
   const dim_t *dstPitch = outT->strides().data();
-  // unsigned int *indicesPitch = (unsigned int *)pindicesPitches;
   const dim_t *indicesPitch = in2T->strides().data();
 
   dim_t srcDimsNum = in1T->ndims();
@@ -100,50 +87,61 @@ INLINE_ATTR void fwdLibGatherInst(LibTensor* outT, LibTensor* in1T, LibTensor* i
   size_t offsetIn = 0;
   for (dim_t i = 0; i < batchedDims; i++)
     offsetIn += srcPitch[i] * coordOut[i];
-  auto index = tIndices[offsetIndices];
+  auto index = indexT[offsetIndices];
+  index = (index < 0) ? index + srcIndex[batchedDims] : index;
   offsetIn += srcPitch[batchedDims] * index;
   for (dim_t i = batchedDims + 1; i < srcDimsNum; i++)
     offsetIn +=
         srcPitch[i] *
         coordOut[indicesDimsNum + i - 1]; // could iterate just until the last
                                           // source non-zero coordInate. todo
-
-  auto posMax = maxRead + initialAddr;
+  size_t posMax = maxRead + initialAddr;
   bool done = false;
   while (!done && (offsetOut < posMax)) {
-    tOutput[offsetOut] = tInput[offsetIn];
+    // Copy chunks of consecutive elements (last dim) until the end of the dimension
+    size_t copySize =
+      (batchedDims + indicesDimsNum < dstDimsNum - 1) ? dstIndex[dstDimsNum - 1] - coordOut[dstDimsNum - 1] : 1;
+    if (offsetOut + copySize > posMax) {
+      // Make sure posMax is not surpassed
+      copySize = posMax - offsetOut;
+    }
+    copyBytes(&srcT[offsetIn * typeSize], &dstT[offsetOut * typeSize], copySize * typeSize);
     // Coordinates are updated to the next position that must be copied.
-    for (size_t j = dstDimsNum - 1; j < dstDimsNum;
-         j--) { // the loop goes from j=Dims-1 to 0, and the wraps around => j > dstDimsNum and ends
-      if (coordOut[j] != (dstIndex[j] - 1)) {
-        offsetOut += dstPitch[j];
-        coordOut[j]++;
-        if (j >= batchedDims + indicesDimsNum)
-          offsetIn += srcPitch[j - indicesDimsNum + 1];
-        else if (batchedDims <= j) {
+    for (int j = dstDimsNum - 1; j >= 0; j--) {
+      if (coordOut[j] + copySize != dstIndex[j]) {
+        offsetOut += dstPitch[j] * copySize;
+        coordOut[j] += copySize;
+        if (j >= (int)(batchedDims + indicesDimsNum)) {
+          offsetIn += srcPitch[j - indicesDimsNum + 1] * copySize;
+        } else if ((int)batchedDims <= j) {
           offsetIndices += indicesPitch[j - batchedDims];
-          offsetIn += (tIndices[offsetIndices] - index) * srcPitch[batchedDims];
-          index = tIndices[offsetIndices];
-        } else
+          auto index_next = indexT[offsetIndices];
+          index_next = (index_next < 0) ? index_next + srcIndex[batchedDims] : index_next;
+          offsetIn += (index_next - index) * srcPitch[batchedDims];
+          index = index_next;
+        } else {
           offsetIn += srcPitch[j];
-        break; // Once the coordinates have been updated, a new copy can be
-               // performed.
+        }
+        break; // Once the coordinates have been updated, a new copy can be performed.
       } else if (j != 0) {
         // previous iteration (of the while loop) was in last element of dest dimension j, reset that dimension for the
         // destination, the source data and the indices.
-        offsetOut -= (dstIndex[j] - 1) * dstPitch[j];
+        offsetOut -= (dstIndex[j] - copySize) * dstPitch[j];
         coordOut[j] = 0;
-        if (j >= batchedDims + indicesDimsNum) {
+        if (j >= (int)(batchedDims + indicesDimsNum)) {
           auto k = j - indicesDimsNum + 1;
-          offsetIn -= (srcIndex[k] - 1) * srcPitch[k];
-        } else if (batchedDims <= j) {
+          offsetIn -= (srcIndex[k] - copySize) * srcPitch[k];
+        } else if ((int)batchedDims <= j) {
           offsetIndices -= (indicesIndex[j - batchedDims] - 1) *
                            indicesPitch[j - batchedDims];
-          offsetIn += (tIndices[offsetIndices] - index) * srcPitch[batchedDims];
-          index = tIndices[offsetIndices];
+          int64_t index_next = indexT[offsetIndices];
+          index_next = (index_next < 0) ? index_next + srcIndex[batchedDims] : index_next;
+          offsetIn += (index_next - index) * srcPitch[batchedDims];
+          index = index_next;
         } else {
           offsetIn -= (srcIndex[j] - 1) * srcPitch[j];
         }
+        copySize = 1; // Reset copy size to default
       } else {
         done = true; // The end of the destination tensor has been reached.
       }
