dda46487334d1992e9a0c51f086db8ed772b108b
Isaac Sánchez Barrera <isaac.sanchez@esperantotech.com>
2024-06-10 16:26:27 +0000

    [dnn-library] [SW-21166] Fix implementation for ScatterData.
    - Fix the implementation for ScatterData.
    
    - Recursion might allow for some optimizations to allow inlining.

--- dnn-library/include/inlining/ScatterDataInst.h
+++ dnn-library/include/inlining/ScatterDataInst.h
@@ -25,45 +25,27 @@ namespace dnn_lib {
 
 namespace inlining {
 
-struct dataToCopyXSliceDim {
-
-  size_t nCopy = 1;
-  size_t advSlice = 1;
-  size_t advDst = 1;
-  size_t sliceAddr = 0;
-  size_t dstAddr = 0;
-  size_t jmpDstAddr = 0;
-  size_t jmpSlcAddr = 0;
-};
-
-template <typename ptrT, dim_t nDim> class WriteSliceToDst {
-public:
-  static INLINE_ATTR void cpyIt(const dataToCopyXSliceDim sliceSteps[max_tensor_dimensions], const ptrT* tSlices,
-                                ptrT* tOutput, dim_t maxDim, size_t& sliceAddr, size_t& dstAddr) {
-    if (nDim >= maxDim) {
-      WriteSliceToDst<ptrT, nDim - 1>::cpyIt(sliceSteps, tSlices, tOutput, maxDim, sliceAddr, dstAddr);
-    }
-    else {
-      for (dim_t k = 0; k < sliceSteps[nDim].nCopy; k++) {
-        WriteSliceToDst<ptrT, nDim - 1>::cpyIt(sliceSteps, tSlices, tOutput, maxDim, sliceAddr, dstAddr);
-        sliceAddr += sliceSteps[nDim].sliceAddr;
-        dstAddr += sliceSteps[nDim].dstAddr;
-      }
+namespace ScatterDataImpl {
+template <typename ElemTy>
+void recurse(dim_array_t& inSliceIdx, dim_t inSliceP, LibTensor* slices, dim_array_t& outSliceIdx, dim_t outSliceP,
+             LibTensor* out) {
+  if (inSliceP == slices->ndims()) {
+    // Copy the element from the input slice to the output
+    // assert(outSliceP == out->ndims())
+    auto OH = out->getHandle<ElemTy>();
+    auto SH = slices->getHandle<ElemTy>();
+    OH.at(outSliceIdx) = SH.at(inSliceIdx);
+  } else {
+    // Since we cannot assume data to be contiguous, set all indices for single element
+    const dim_t* inSlicesDims = slices->dims().data();
+    for (size_t i = 0; i < inSlicesDims[inSliceP]; ++i) {
+      inSliceIdx[inSliceP] = i;
+      outSliceIdx[outSliceP] = i;
+      recurse<ElemTy>(inSliceIdx, inSliceP + 1, slices, outSliceIdx, outSliceP + 1, out);
     }
   }
-};
-
-template <typename ptrT>
-class WriteSliceToDst<ptrT, 0> {
-  public:
-    static INLINE_ATTR void cpyIt(const dataToCopyXSliceDim sliceSteps[max_tensor_dimensions], const ptrT* tSlices,
-                                  ptrT* tOutput, [[maybe_unused]] size_t maxDim, size_t& sliceAddr, size_t& dstAddr) {
-      for (size_t k = 0; k < sliceSteps[0].nCopy; k++) {
-        auto val = tSlices[(sliceAddr + k)];
-        tOutput[(dstAddr + k)] = val;
-      }
-    }
-};
+}
+} // namespace ScatterDataImpl
 
 template <ElemKind elK>
 INLINE_ATTR void fwdLibScatterDataInst(LibTensor* outT, LibTensor* in1T, LibTensor* in2T,
@@ -76,123 +58,36 @@ INLINE_ATTR void fwdLibScatterDataInst(LibTensor* outT, LibTensor* in1T, LibTens
   /* maintain compatibility through the new Iface Libtensor */
   /* outT --> dst  in1T--> src in2T--> slice*/
 
-  auto tSlices = in2T->getRawDataPointer<srcType>();
-  auto tOutput = outT->getRawDataPointer<srcType>();
-  auto tIndices = in1T->getRawDataPointer<uint64_t>();
-
-  const dim_t *dstIndex = outT->dims().data();
   const dim_t *indicesIndex = in1T->dims().data();
-  const dim_t *slicesIndex = in2T->dims().data();
-  const dim_t *dstPitch = outT->strides().data();
-  const dim_t *indicesPitch = in1T->strides().data();
-  const dim_t* slicesPitch = in2T->strides().data();
 
-  dim_t sliceNumDim = in2T->ndims();
+  dim_t sliceNumDim = in1T->ndims();
 
-  dataToCopyXSliceDim sliceSteps[max_tensor_dimensions];
-  size_t push_ptr = 0;
+  dim_array_t slicesIt = in1T->dims();
+  slicesIt[sliceNumDim - 1] = 1;
+  dim_array_t sliceIdx;
+  dim_array_t outIdx;
 
-  //assert(sliceNumDim>1);
-  size_t i = 0;
-  size_t accumSlc = 0;
-  size_t accumDst = 0;
-
-  for (i = 0; i < (sliceNumDim - 1); i++) {
-
-    sliceSteps[push_ptr++] = dataToCopyXSliceDim();
-    sliceSteps[i].nCopy = slicesIndex[sliceNumDim-(i+1)];
-    
-    accumSlc = 0;
-    accumDst = 0;
-
-    sliceSteps[i].advSlice = slicesPitch[(sliceNumDim-(i+1))];
-    sliceSteps[i].advDst = dstPitch[(sliceNumDim-(i+1))];
-    sliceSteps[i].jmpSlcAddr = sliceSteps[i].nCopy * sliceSteps[i].advSlice;
-    sliceSteps[i].jmpDstAddr = sliceSteps[i].nCopy * sliceSteps[i].advDst;
-    
-    if (i == 1) {
-      sliceSteps[i].sliceAddr = sliceSteps[i].advSlice;
-      sliceSteps[i].dstAddr = sliceSteps[i].advDst;
-    }
-    else if (i>1) {
-
-      for (dim_t j = 1; j < i; j++) {
-        accumSlc += sliceSteps[j].jmpSlcAddr;
-        accumDst += sliceSteps[j].jmpDstAddr;
+  auto IH = in1T->getHandle<uint64_t>();
+  for (dim_t& k0 = sliceIdx[0] = 0; k0 < slicesIt[0]; ++k0) {
+    for (dim_t& k1 = sliceIdx[1] = 0; k1 < slicesIt[1]; ++k1) {
+      for (dim_t& k2 = sliceIdx[2] = 0; k2 < slicesIt[2]; ++k2) {
+        for (dim_t& k3 = sliceIdx[3] = 0; k3 < slicesIt[3]; ++k3) {
+          for (dim_t& k4 = sliceIdx[4] = 0; k4 < slicesIt[4]; ++k4) {
+            for (dim_t& k5 = sliceIdx[5] = 0; k5 < slicesIt[5]; ++k5) {
+              for (dim_t i = 0; i < indicesIndex[sliceNumDim - 1]; ++i) {
+                // Set the first indicesIndex[sliceNumDim - 1] indices of the output slice
+                sliceIdx[sliceNumDim - 1] = i;
+                outIdx[i] = IH.at(sliceIdx);
+              }
+              ScatterDataImpl::recurse<srcType>(sliceIdx, sliceNumDim - 1, in2T, outIdx, indicesIndex[sliceNumDim - 1],
+                                                outT);
+            }
+          }
+        }
       }
-
-      sliceSteps[i].sliceAddr = sliceSteps[i].advSlice - accumSlc;
-      sliceSteps[i].dstAddr = sliceSteps[i].advDst - accumDst;
     }
-  }
-
-  for (i = 0; i < indicesIndex[0]; i++) {
-    dim_t dstDataIdx = 0;
-    for (dim_t j = 0; j < indicesIndex[1]; j++) {
-      dstDataIdx *= dstIndex[j];
-      dstDataIdx += tIndices[(i*indicesPitch[0])+(j*indicesPitch[1])];
-    }
-
-    size_t dstAddr = (dstDataIdx * dstPitch[0]);
-    size_t sliceAddr = (i * slicesPitch[0]);
-
-    //  Non-recursive version keep it just in case of the 
-    //  lower recursive perfomance 
-    //
-    // for (unsigned int k5 = 0; k5 < sliceSteps[5].nCopy; k5++) {
-    //   for (unsigned int k4 = 0; k4 < sliceSteps[4].nCopy; k4++) {
-    //  for (unsigned int k3 = 0; k3 < sliceSteps[3].nCopy; k3++) {
-    //    for (unsigned int k2 = 0; k2 < sliceSteps[2].nCopy; k2++) {
-    //      for (unsigned int k1 = 0; k1 < sliceSteps[1].nCopy; k1++) {
-    //        for (unsigned int k0 = 0; k0 < sliceSteps[0].nCopy; k0++) {
-    //    auto val = tSlices[(sliceAddr + k0)];
-    //    tOutput[(dstAddr + k0)] = val;
-    //        }
-    //        sliceAddr += sliceSteps[1].sliceAddr;
-    //        dstAddr += sliceSteps[1].dstAddr;
-    //      }
-    //      sliceAddr += sliceSteps[2].sliceAddr;
-    //      dstAddr += sliceSteps[2].dstAddr;
-    //    }
-    //    sliceAddr += sliceSteps[3].sliceAddr;
-    //    dstAddr += sliceSteps[3].dstAddr;
-    //  }
-    //  sliceAddr += sliceSteps[4].sliceAddr;
-    //  dstAddr += sliceSteps[4].dstAddr;
-    //   }
-    //   sliceAddr += sliceSteps[5].sliceAddr;
-    //   dstAddr += sliceSteps[5].dstAddr;
-    // }
-
-    WriteSliceToDst<srcType, (max_tensor_dimensions-1)>::cpyIt(sliceSteps, tSlices, tOutput, sliceNumDim-1, sliceAddr, dstAddr);
   }
 }
-
-// template <typename ElemTy>
-// void BoundInterpreterFunction::fwdScatterDataInstCopyImpl() {
-//   Tensor *dataT = getTensor(I->getData());
-//   Tensor *indicesT = getTensor(I->getIndices());
-//   Tensor *slicesT = getTensor(I->getSlices());
-
-//   assert(indicesT->dims().size() == 2 &&
-//          "Index should be stored in 2D tensor!");
-//   const dim_t dataSliceSize = slicesT->size() / slicesT->dims()[0] *
-//                               slicesT->getType().getElementSize();
-
-//   auto IH = indicesT->getHandle<int64_t>();
-//   // For each index, copy from the slice at that index into the location in data
-//   // given the offset from the indices tensor.
-//   for (dim_t i = 0, end = indicesT->dims()[0]; i < end; i++) {
-//     dim_t destDataIdx = 0;
-//     for (dim_t j = 0, e = indicesT->dims()[1]; j < e; j++) {
-//       destDataIdx *= dataT->dims()[j];
-//       destDataIdx += IH.at({i, j});
-//     }
-//     std::copy(&slicesT->getUnsafePtr()[i * dataSliceSize],
-//               &slicesT->getUnsafePtr()[(i + 1) * dataSliceSize],
-//               &dataT->getUnsafePtr()[dataSliceSize * destDataIdx]);
-//   }
-// } 
 
 } // namespace inlining
 
