464ebf740c41465935cd6069aaf1c3279eb9393b
Roman Shaposhnik <roman@nekko.ai>
2025-10-17 18:59:44 -0700

    [dnn-library] Making Esperanto work available under Apache License v2

--- dnn-library/include/inlining/SplatInst.h
+++ dnn-library/include/inlining/SplatInst.h
@@ -1,11 +1,6 @@
 /*-------------------------------------------------------------------------
- * Copyright (C) 2021, Esperanto Technologies Inc.
- * The copyright to the computer program(s) herein is the
- * property of Esperanto Technologies, Inc. All Rights Reserved.
- * The program(s) may be used and/or copied only with
- * the written permission of Esperanto Technologies and
- * in accordance with the terms and conditions stipulated in the
- * agreement/contract under which the program(s) have been supplied.
+ * Copyright (c) 2025 Ainekko, Co.
+ * SPDX-License-Identifier: Apache-2.0
  *-------------------------------------------------------------------------
  */
 
